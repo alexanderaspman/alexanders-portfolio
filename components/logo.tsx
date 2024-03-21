67128ff3133107bd9c12603be8868c25346965f2
Alexander Aspman <aspmanalexander@gmail.com>
2024-03-21 09:36:49 +0100

    add://added hardcoded href, not responsive

---
+++ components/logo.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image"
+
+const Logo=()=>{
+    return(
+        <Image
+          src="https://media.crystallize.com/alexanderaspmannu/23/7/12/2/@200/logo.avif"
+          alt="Logo"
+          width={70}
+          height={70}
+          style={{margin: '0 20px'}}
+          
+        />
+   )
+}
+export default Logo
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-
+  
+    images: {
+      domains: ['media.crystallize.com'], // Add your domain here
+    },
+  }
+  
+  
+  
 export default nextConfig;
